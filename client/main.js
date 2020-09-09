f792d9086eb9b2ca032176fceadd09856d91c3cc
Rafael Atheya <rafael.satriabhi@gmail.com>
2020-09-10 01:06:47 +0700

    Axionya masih belum bisa jalan

--- client/main.js
+++ client/main.js
@@ -1,17 +1,42 @@
+const baseUrl = "http://localhost:3000"
+
 new Vue ({
     el:"#app",
     data:{
+        currentPage: "main-page",
+        user:{
+            email: "",
+            password: ""
+        },
         msg:"Hello Vue",
         newTodo:"",
         todoList: [],
         onGoing:[]
     },
     methods:{
+        login(){
+            localStorage.setItem(`access_token`,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2F0cmlhYmhpIiwiZW1haWwiOiJzYXRyaWFiaGlAaGVrdGl2OS5jb20iLCJvcmdhbml6YXRpb25fbmFtZSI6Ikhla3RpdjkiLCJpYXQiOjE1OTk2NzQxMTh9.Pf3mDD3t6b6CFa_Y5HzMMzC8mAhhBy7JoaZjGk5I6Q8")
+        },
+        logout(){
+            localStorage.removeItem(`access_token`)
+        },
         addTodo(){
             this.todoList.push(this.newTodo)
         },
         processTodo(){
             
+        },
+        fetchTodos(){
+            axios({
+                methods: "get",
+                url: `${baseUrl}/todo`
+            })
+            .then(data=>{
+                console.log(data)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
         }
     }
 })
--- server/app.js
+++ server/app.js
@@ -2,10 +2,11 @@ const express = require(`express`)
 const app = express()
 const port = 3000
 const routerIndex = require(`./routes/index.js`)
+const cors = require(`cors`)
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-
+app.use(cors())
 app.use(`/`,routerIndex)
 
 
